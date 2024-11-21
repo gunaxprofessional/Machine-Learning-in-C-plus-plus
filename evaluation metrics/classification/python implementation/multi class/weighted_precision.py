38042b13b3cc69b3755186d67e97d93d09240481
Guna M <66107066+gunaxprofessional@users.noreply.github.com>
2024-11-21 07:41:59 +0530

    mutli-class done expect f1_score

---
+++ evaluation metrics/classification/python implementation/multi class/weighted_precision.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import precision_score, recall_score, f1_score
+y_true = [1, 0, 1, 2, 0, 1, 0, 0, 1, 2]
+y_pred = [0, 0, 0, 2, 0, 1, 0, 1, 2, 2]
+score = precision_score(y_true, y_pred, average='weighted')
+score1 = recall_score(y_true, y_pred, average='weighted')
+f1_score = f1_score(y_true, y_pred, average='weighted')
+print("Precision score: ", score)
+print("Recall score: ", score1)
+print("F1 Score: ", f1_score)
